81fa539c9149bfd2d0d8c44fde9702d7dc651209
Victor Anderssén <victor.anderssen@gmail.com>
2024-12-26 14:41:57 +0200

    FPS

--- dungeon_game/src/com/victoranderssen/dungeon_game/Game.java
+++ dungeon_game/src/com/victoranderssen/dungeon_game/Game.java
@@ -13,69 +13,87 @@ import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
-	
+
 	public static final String NAME = "Dungeon Game";
 	public static final int HEIGHT = 360;
-	public static final int WIDTH = HEIGHT *16 / 9;
-	
+	public static final int WIDTH = HEIGHT * 16 / 9;
+
 	private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-	
+
 	WritableRaster raster = image.getRaster();
-	private DataBufferInt dataBufferInt = ((DataBufferInt)image.getRaster().getDataBuffer());
-	private int[] pixels = dataBufferInt.getData();	
-	
+	private DataBufferInt dataBufferInt = ((DataBufferInt) image.getRaster().getDataBuffer());
+	private int[] pixels = dataBufferInt.getData();
+
 	private boolean running = false;
-	
+	private int tickCount;
+
 	public void start() {
 		running = true;
 		new Thread(this).start();
 	}
-	
+
 	public void stop() {
 		running = false;
 	}
-	
+
 	public void run() {
+		long lastTime = System.nanoTime();
+		double unprocessed = 0;
+		double nsPerTick = 1000000000.0 / 60;
+		int frames = 0;
+		long lastTimer1 = System.currentTimeMillis();
+
 		while (running) {
-			runStep();
+			long now = System.nanoTime();
+			unprocessed += (now - lastTime) / nsPerTick;
+			lastTime = now;
+
+			while (unprocessed >= 1) {
+				tick();
+				unprocessed--;
+			}
+
+			{
+				frames++;
+				render();
+			}
+
+			if (System.currentTimeMillis() - lastTimer1 > 1000) {
+				lastTimer1 += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+
 		}
 	}
-	
-	private void runStep() {
-		tick();
-		render();
-	}
-	
-	int tickCount;
-	
+
 	public void tick() {
 		tickCount++;
 	}
-	
+
 	public void render() {
-		BufferStrategy bs =  this.getBufferStrategy();
-		if (bs == null)
-		{
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
 			createBufferStrategy(3);
 			return;
 		}
-			
+
 		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = i + tickCount;
 		}
-	
+
 		Graphics g = bs.getDrawGraphics();
 		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		g.dispose();
 		bs.show();
 	}
-	
+
 	public static void main(String[] args) {
 		Game game = new Game();
-		game.setMinimumSize(new Dimension(WIDTH*2, HEIGHT*2));
-		game.setMaximumSize(new Dimension(WIDTH*2, HEIGHT*2));
-		game.setPreferredSize(new Dimension(WIDTH*2, HEIGHT*2));
-		
+		game.setMinimumSize(new Dimension(WIDTH * 2, HEIGHT * 2));
+		game.setMaximumSize(new Dimension(WIDTH * 2, HEIGHT * 2));
+		game.setPreferredSize(new Dimension(WIDTH * 2, HEIGHT * 2));
+
 		JFrame frame = new JFrame(Game.NAME);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(new BorderLayout());
@@ -84,7 +102,7 @@ public class Game extends Canvas implements Runnable {
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-		
+
 		game.start();
 	}
 }
